a3342828854b5a569fb66a871e00feba306518ac
jinhyun <free1330@gmail.com>
2016-06-15 13:27:10 +0900

    framework: init

---
+++ src/main/java/com/cocotask/core/CoreApplication.java
@@ -0,0 +1,13 @@
+package com.cocotask.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.cocotask")
+public class CoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+}
